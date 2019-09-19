62df99933a9a61385732526341287fc0a4363c24
ozawa.seijiro <seijiro4751@gmail.com>
2019-09-19 19:00:56 +0900

    company-mode

--- init.el
+++ init.el
@@ -69,6 +69,9 @@
     ;; flyspell
     flyspell
 
+    ;; jump
+    dumb-jump
+
     )
   )
 
@@ -77,14 +80,21 @@
     (package-install package)))
 
 (require 'company)
-(global-company-mode) ; 全バッファで有効にする 
+(global-company-mode) ; 全バッファで有効にする
+(setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
 (setq company-idle-delay 0) ; デフォルトは0.5
 (setq company-minimum-prefix-length 2) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(setq completion-ignore-case t)
+(setq company-dabbrev-downcase nil)
+
 (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
 (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
-
-     
+(define-key company-active-map (kbd "C-s") 'company-filter-candidates) ;; C-sで絞り込む
+(define-key company-active-map (kbd "C-i") 'company-complete-selection) ;; TABで候補を設定
+(define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
+(define-key company-active-map (kbd "C-f") 'company-complete-selection) ;; C-fで候補を設定
+(define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete) ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
 
 (require 'eglot)
 
