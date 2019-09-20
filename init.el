57f1a73368421f8016874a89dacdd7c7c646b9c0
ozawa.seijiro <seijiro4751@gmail.com>
2019-09-20 19:39:21 +0900

    company-delay

--- init.el
+++ init.el
@@ -100,7 +100,7 @@
 (require 'company)
 (global-company-mode) ; 全バッファで有効にする
 (setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
-(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-idle-delay 0.8) ; デフォルトは0.5
 (setq company-minimum-prefix-length 2) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
 (setq completion-ignore-case t)
