957ee58fbbd66755c5dcf614ed921b255aa205d1
ozawaseijiro <seijiro4751@gmail.com>
2020-01-29 21:57:33 +0900

    fix

--- init.el
+++ init.el
@@ -1009,7 +1009,8 @@ If there are two or more windows, it will go to another window."
   
   (setq exec-path (parse-colon-path (getenv "GOROOT")))
   (setq exec-path (parse-colon-path (getenv "GOPATH")))
-  (setq exec-path (parse-colon-path (getenv "GOPATH")))
+  (setq exec-path (parse-colon-path (getenv "PATH")))
+  
   (setq eshell-path-env (getenv "GOPATH"))
   (setq eshell-path-env (getenv "PATH"))
   (setq eshell-path-env (getenv "GOROOT"))  
@@ -1017,7 +1018,6 @@ If there are two or more windows, it will go to another window."
   ("M-." . 'godef-jump)
   ("M-," . 'pop-tag-mark)
   )
-
 
 (use-package company-go
   :straight t
