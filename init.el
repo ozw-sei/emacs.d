0e55d20ed572ed41ff3c8827b3ef9d727fd78d7b
ozw-sei <seijiro4751@gmail.com>
2020-02-14 11:36:37 +0900

    define origami-hyra

--- init.el
+++ init.el
@@ -54,7 +54,21 @@
 ;;   :config (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode))
 
 (use-package origami
-  :straight t)
+  :straight t
+  :config
+  (defhydra hydra-folding (:color red)
+    "
+  _o_pen node    _n_ext fold       toggle _f_orward
+  _c_lose node   _p_revious fold   toggle _a_ll
+  "
+    ("o" origami-open-node)
+    ("c" origami-close-node)
+    ("n" origami-next-fold)
+    ("p" origami-previous-fold)
+    ("f" origami-forward-toggle-node)
+    ("a" origami-toggle-all-nodes))
+
+(bind-key "<tab>" 'hydra-folding/body))
 
 ;; Or if you use use-package
 (use-package dashboard
