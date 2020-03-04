f498c8f12a75098721b17c63cbf9d17fe3d2b56b
ozw-sei <seijiro4751@gmail.com>
2020-03-04 14:24:05 +0900

    chore

--- init.el
+++ init.el
@@ -560,15 +560,15 @@
 
 (defhydra hydra-projectile nil
   "Projectile"
-  ("f"   counsel-projectile-find-file         "Find File")
+  ("f"   counsel-projectile-find-file         "Find File" :exit t)
   ("A"   projectile-ag                "ag" :exit t)
   ("a"   counsel-projectile-ag                "ag in counsel" :exit t)
   ("r"   projectile-recentf                  "Recent Files" :exit t)
-  ("d"   counsel-projectile-find-dir                 "Find Directory")
+  ("d"   counsel-projectile-find-dir                 "Find Directory" :exit t)
   ("b"   counsel-projectile-switch-to-buffer         "Switch to Buffer")
   ("c" counsel-compile "compile-project" :exit t)
   ("s"   counsel-projectile-switch-project           "Switch Project")
-  ("k"   projectile-kill-buffers             "Kill Buffers"))
+  ("k"   projectile-kill-buffers             "Kill Buffers" :exit t))
 
 (bind-key "C-c p" 'hydra-projectile/body)
 (bind-key "C-c C-p" 'hydra-projectile/body)
