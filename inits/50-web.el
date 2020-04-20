3317118ccf3c0106c672550274d8a8e2289768f8
ozawa.seijiro <seijiro4751@gmail.com>
2020-04-20 17:09:44 +0900

    configure eje to web-mode

--- inits/50-web.el
+++ inits/50-web.el
@@ -2,7 +2,7 @@
   :straight t
   :config
   (add-to-list 'auto-mode-alist '("\\.html$" . web-mode)))
-                                        ;(add-to-list 'auto-mode-alist '("\\.js$" . web-mode)) ;
+                                        (add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.ts$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
