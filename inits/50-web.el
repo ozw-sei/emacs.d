9fc55e17cd1c8447ab75a316d41ed53939ea5068
ozawa.seijiro <seijiro4751@gmail.com>
2020-04-22 10:08:54 +0900

    fix-reindent

--- inits/50-web.el
+++ inits/50-web.el
@@ -2,8 +2,8 @@
   :straight t
   :config
   (add-to-list 'auto-mode-alist '("\\.html$" . web-mode)))
-                                        (add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
-                                        (add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.ts$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
                                         ;(setq web-mode-content-types-alist
