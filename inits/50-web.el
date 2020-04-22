738b94168b71ef7439f85fb5b1fe869f41a69e9f
ozawa.seijiro <seijiro4751@gmail.com>
2020-04-22 10:08:21 +0900

    configure web-mode jsp

--- inits/50-web.el
+++ inits/50-web.el
@@ -3,7 +3,7 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.html$" . web-mode)))
                                         (add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
-                                        ;(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+                                        (add-to-list 'auto-mode-alist '("\\.jsp$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.ts$" . web-mode))
                                         ;(add-to-list 'auto-mode-alist '("\\.tsx$" . web-mode))
                                         ;(setq web-mode-content-types-alist
