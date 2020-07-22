ce3e1ffaccbe27c413949ee5e58c65422cd8f258
ozw-sei <seijiro4751@gmail.com>
2020-07-22 10:12:05 +0900

    ctrlf to swiper

--- inits/10-counsel.el
+++ inits/10-counsel.el
@@ -51,10 +51,16 @@
 
 (bind-key* "M-y" 'yank-pop+)
 
-(use-package ctrlf
+(use-package swiper
   :straight t
   :config
-  (ctrlf-mode +1))
+  (defun isearch-forward-or-swiper (use-swiper)
+    (interactive "p")
+    ;; (interactive "P") ;; 大文字のPだと，C-u C-sでないと効かない
+    (let (current-prefix-arg)
+      (call-interactively (if use-swiper 'swiper 'isearch-forward))))
+  (global-set-key (kbd "C-s") 'isearch-forward-or-swiper)
+  )
 
 (use-package selected
   :straight t
