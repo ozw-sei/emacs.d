8628ca9b22ca510caf014afc24c10586c4c87feb
ozawaseijiro <seijiro4751@gmail.com>
2020-09-14 01:05:01 +0900

    import shackle

--- inits/10-counsel.el
+++ inits/10-counsel.el
@@ -36,8 +36,36 @@
 
   (global-set-key (kbd "C-x f") 'helm-find-files)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  )
+)
 
+(use-package shackle
+  :straight t
+  :config
+  (setq shackle-rules
+      '(;; *compilation*は下部に2割の大きさで表示
+        (compilation-mode :align below :ratio 0.2)
+        ;; ヘルプバッファは右側に表示
+        ("*Help*" :align right)
+        ;; 補完バッファは下部に3割の大きさで表示
+        ("*Completions*" :align below :ratio 0.3)
+        ;; M-x helm-miniは下部に7割の大きさで表示
+        ("*helm mini*" :align below :ratio 0.7)
+        ;; 他のhelmコマンドは右側に表示 (バッファ名の正規表現マッチ)
+        ("\*helm" :regexp t :align bottom)
+        ;; 上部に表示
+        ("foo" :align above)
+        ;; 別フレームで表示
+        ("bar" :frame t)
+        ;; 同じウィンドウで表示
+        ("baz" :same t)
+        ;; ポップアップで表示
+        ("hoge" :popup t)
+        ;; 選択する
+        ("abc" :select t)
+        ))
+  (setq helm-display-function 'pop-to-buffer) ; make helm play nice
+  (shackle-mode 1)
+  )
 
 (use-package helm-selected
   :straight t
