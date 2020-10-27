79d2aea251c44d41f19d7f4505fead34c55582f4
ozawaseijiro <seijiro4751@gmail.com>
2020-10-27 23:23:06 +0900

    Revert "Revert "fix""
    This reverts commit 931cca5b34ae5fbdb9681bdc786ba665641ac81a.

--- inits/20-company-mode.el
+++ inits/20-company-mode.el
@@ -11,6 +11,12 @@
   (company-dabbrev-downcase nil)
   (company-idle-delay 0.3)
 
+  :bind
+  (
+   ("M-/" . company-complete)
+   ("C-<tab>" . company-capf)
+   )
+
   :config
   (global-company-mode) ; 全バッファで有効にする
   (company-mode 1)
@@ -20,9 +26,7 @@
   (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
   (define-key company-active-map (kbd "C-i") 'company-complete-selection)
   (define-key company-active-map [tab] 'company-complete-selection)
-  (define-key company-active-map (kbd "C-f") 'company-complete-selection)
-  (define-key emacs-lisp-mode-map (kbd "M-/") 'company-complete)
-  (define-key emacs-lisp-mode-map (kbd "C-<tab>") 'company-capf))
+  (define-key company-active-map (kbd "C-f") 'company-complete-selection))
 
 
 
