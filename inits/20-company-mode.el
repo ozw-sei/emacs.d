79261eea572af4966f16a51d0cdb55c538532a9e
ozawaseijiro <seijiro4751@gmail.com>
2020-10-27 23:20:52 +0900

    fix

--- inits/20-company-mode.el
+++ inits/20-company-mode.el
@@ -22,42 +22,7 @@
   (define-key company-active-map [tab] 'company-complete-selection)
   (define-key company-active-map (kbd "C-f") 'company-complete-selection)
   (define-key emacs-lisp-mode-map (kbd "M-/") 'company-complete)
-  (define-key emacs-lisp-mode-map (kbd "C-<tab>") 'company-capf)
-
-  (defvar company-mode/enable-yas t)
-  (defun company-mode/backend-with-yas (backend)
-    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
-        backend
-      (append (if (consp backend) backend (list backend))
-              '(:with company-yasnippet))))
-
-  (add-to-list 'company-backends '(company-bbdb
-                                   company-nxml
-                                   company-css
-                                   company-eclim
-                                   company-semantic
-                                   company-clang
-                                   company-xcode
-                                   company-cmake
-                                   company-capf
-                                   company-dabbrev-code
-                                   company-gtags
-                                   company-etags
-                                   company-keywords
-                                   company-oddmuse
-                                   company-files
-                                   company-dabbrev
-                                   mapcar
-                                   company-mode/backend-with-yas
-                                   company-irony
-                                   company-c-headers
-                                   company-auctex
-                                   company-math-symbols-unicode
-                                   company-lsp
-                                   company-elisp
-                                   company-yasnippet)
-               )
-  )
+  (define-key emacs-lisp-mode-map (kbd "C-<tab>") 'company-capf))
 
 
 
