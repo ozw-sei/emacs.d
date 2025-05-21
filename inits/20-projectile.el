7103bb68992d99cc866302184575e85fc5907471
ozw-sei <seijiro4751@gmail.com>
2025-05-21 10:21:46 +0900

    fix

--- inits/20-projectile.el
+++ inits/20-projectile.el
@@ -36,32 +36,6 @@
   :straight t
   :ensure t)
 
-(use-package consult-ripgrep
-  :straight t
-  :ensure t)
-
-(use-package consult-make
-  :straight t
-  :ensure t)
-
-(use-package projectile-consult-bindings ;; Renamed from helm-projectile
-  :straight t ;; This might be redundant if it's just for config, but keep for structure
-  :after (projectile consult) ;; Ensure projectile and consult are loaded
-  :config
-  (define-key projectile-mode-map [remap projectile-find-other-file] #'projectile-find-other-file)
-  (define-key projectile-mode-map [remap projectile-find-file] #'projectile-find-file)
-  (define-key projectile-mode-map [remap projectile-find-file-in-known-projects] #'projectile-find-file-in-known-projects)
-  (define-key projectile-mode-map [remap projectile-find-file-dwim] #'projectile-find-file-dwim)
-  (define-key projectile-mode-map [remap projectile-find-dir] #'projectile-find-dir)
-  (define-key projectile-mode-map [remap projectile-recentf] #'consult-recent-file)
-  (define-key projectile-mode-map [remap projectile-switch-to-buffer] #'consult-buffer)
-  (define-key projectile-mode-map [remap projectile-grep] #'consult-ripgrep) ;; Changed from consult-grep to consult-ripgrep
-  (define-key projectile-mode-map [remap projectile-ack] #'consult-ripgrep)
-  (define-key projectile-mode-map [remap projectile-ag] #'consult-ripgrep)
-  (define-key projectile-mode-map [remap projectile-ripgrep] #'consult-ripgrep)
-  ;; (define-key projectile-mode-map [remap projectile-browse-dirty-projects] #'helm-projectile-browse-dirty-projects) ;; Commented out
-  )
-
 (defhydra hydra-projectile nil
   "Projectile"
   ("f"   projectile-find-file         "Find File" :exit t)
@@ -69,7 +43,7 @@
   ("a"   consult-ripgrep              "Ripgrep (ag)" :exit t)
   ("r"   consult-recent-file          "Recent Files" :exit t)
   ("d"   projectile-find-dir          "Find Directory" :exit t)
-  ("b"   consult-buffer               "Switch to Buffer" :exit t) ;; Added :exit t for consistency
+  ("b"   nsult-buffer               "Switch to Buffer" :exit t) ;; Added :exit t for consistency
   ("k"   projectile-kill-buffers      "Kill Buffers" :exit t)
   ("R"   projectile-replace           "Replace" :exit t)
   ("s"   projectile-switch-project    "Switch Project" :exit t)
@@ -82,9 +56,6 @@
   ("p"   persp-prev                   "Prev Perspective") ;; Clarified label
   ;; ("k"   projectile-kill-buffers             "Kill Buffers" :exit t) ;; Duplicate k, removed
   )
-
-;; (use-package helm-ag
-;;   :straight t)
 
 (bind-key* "C-c p" 'hydra-projectile/body)
 (bind-key* "C-c C-p" 'hydra-projectile/body)
